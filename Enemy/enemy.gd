6cf8536a5a19793cad8d215d9caf30ea13e593ea
DTNevaeha <DTNevaeha@gmail.com>
2024-05-22 14:32:17 -0500

    Enemies can now be killed

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,8 +1,19 @@
 extends PathFollow3D
 
+@export var max_health: int = 50
 @export var speed: float = 2.5
 
 @onready var base = get_tree().get_first_node_in_group("base")
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		if current_health < 1:
+			queue_free()
+
+
+func _ready() -> void:
+	current_health = max_health
 
 
 func _process(delta: float) -> void:
